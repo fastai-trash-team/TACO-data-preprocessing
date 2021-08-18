da86186c5d15fae2b9433e4058ee0847d183acae
Stan <spotums@gmail.com>
2021-08-18 19:37:20 +1000

    Refactoring
    Move logic into move_images() function
    Add docstring
    Change the while loop in move_images() into a for loop

--- yolo-move-images.py
+++ yolo-move-images.py
@@ -1,16 +1,21 @@
-########################################
-# 1. Move images to one folder and rename them, the renaming format is already reflected in the yolov5 annotations creation script
-########################################
+"""
+Move images to one folder and rename them to avoid duplicate names
+The renaming format is already reflected in the yolov5 annotations creation script so annotations will match the new folder/name structure
+"""
 
 import shutil
 import os
 from os.path import exists
 
-if not exists(base_path + 'images'):
-    os.mkdir(base_path + 'images')
+def move_images():
+    """
+    Moves images from 'data/batch_n/' to 'images/' and renames them using a batch_id prefix to ensure unique naming
+    """
+    if not exists(base_path + 'images'):
+        os.mkdir(base_path + 'images')
 
-batch_id = 1
-while batch_id < 16:
-  batchdir = os.listdir(f'data/batch_{batch_id}')
-  [shutil.move(f'data/batch_{batch_id}/{o}', f'images/b{batch_id}_{o}') for o in batchdir if o[-3:].lower() == 'jpg']
-  batch_id += 1
+    for batch_id in range(1,16):
+        batchdir = os.listdir(f'data/batch_{batch_id}')
+        [shutil.move(f'data/batch_{batch_id}/{o}', f'images/b{batch_id}_{o}') for o in batchdir if o[-3:].lower() == 'jpg']
+        
+move_images()
